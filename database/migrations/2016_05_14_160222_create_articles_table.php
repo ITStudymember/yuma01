368e253a66ba1d4394ef4f469455599bad2404dc
木村友真 <y.kimura.0608@gmail.com>
2016-05-15 01:05:43 +0900

    articlesテーブルの作成

--- database/migrations/2016_05_14_160222_create_articles_table.php
+++ database/migrations/2016_05_14_160222_create_articles_table.php
@@ -14,6 +14,8 @@ class CreateArticlesTable extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->increments('id');
+            $table->text('title');
+            $table->text('body');
             $table->timestamps();
         });
     }
