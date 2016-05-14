53bacb33aed98316215ad8ffe8e0872aaa1a8375
木村友真 <y.kimura.0608@gmail.com>
2016-05-15 01:23:54 +0900

    記事の投稿機能実装

--- app/Http/Controllers/BlogsController.php
+++ app/Http/Controllers/BlogsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Article;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -10,10 +11,21 @@ class BlogsController extends Controller
 {
     //
     /**
+     * 新しい記事の登録
+     *
+     * @param $request
      * @return mixed
+     *
      */
-    public function newBlog()
+    public function postArticle(Request $request)
     {
-        return view('new_blog');
+        $article = new Article();
+
+        $article->title = $request->title;
+        $article->body = $request->body;
+
+        $article->save();
+
+        return redirect('/');
     }
 }
--- resources/views/new_blog.blade.php
+++ resources/views/new_blog.blade.php
@@ -1,17 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">タイトル</div>
+<form action="/new_blog" method="post">
+    {{ csrf_field() }}
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="panel panel-default">
+                    <div class="panel-heading">タイトル</div>
 
-                <div class="panel-body">
-                    <input type="text" name="title">
+                    <div class="panel-body">
+                        <input type="text" name="title">
+                    </div>
+                    <div class="panel-heading">記事の内容</div>
+
+                    <div class="panel-body">
+                        <textarea name="body"></textarea>
+                    </div>
                 </div>
             </div>
+            <input type="submit" value="投稿する">
         </div>
     </div>
-</div>
+</form>
 @endsection
