ad611ac6cbda744b5fd3d62d7aa7d93da4e5c229
木村友真 <y.kimura.0608@gmail.com>
2016-05-18 22:30:04 +0900

    top画像の表示

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,15 @@
 @extends('layouts.app')
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
+@section('link')
+    <link rel="stylesheet" href="/css/common.css">
+    <link rel="stylesheet" href="/css/welcome.css">
+@endsection
 
-                <div class="panel-body">
-                    Your Application's Landing Page.
-                </div>
-            </div>
-        </div>
+@section('content')
+    <div class="top_img_area">
+        <img class="top_img" src="/images/site_images/top.png">
     </div>
-</div>
+    @foreach($articles as $article)
+        <p><a href="/{{ $article->id }}">{{ $article->title }}</a></p>
+    @endforeach
 @endsection
